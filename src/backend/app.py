6117fc4b3e88fbb974c08e40c31d0ee5663abe02
Murli Loka <mnloka@gmail.com>
2019-08-30 10:04:25 -0400

    Enabled swagger api documentation
    This change enables testing of backend api with swagger web app - builtin flask app

--- src/backend/app.py
+++ src/backend/app.py
@@ -8,6 +8,7 @@ from joblib import dump, load
 import os
 from time import time
 import numpy as np
+import sklearn
 
 flask_app = Flask(__name__)
 app = Api(app = flask_app, 
@@ -15,6 +16,24 @@ app = Api(app = flask_app,
 		  title = "Lending Prediction Tool", 
 		  description = "Tools for Lender Simulation and Evaluation")
 
+name_space = app.namespace('api', description='Simulator and Evaluator Lending support Tools')
+
+simulatorModel = app.model('Simulator Model', 
+				  {'input': fields.List(fields.Integer(required = True, 
+														default = "1,1,4,3,3,1,2,6,5,4",
+														description="Use this integer array of size 10 to test Simulator model", 
+														help="Input Integer array cannot be blank or size less than 10."))})
+
+evaluatorModel = app.model('Evaluator Model', 
+				  {'input': fields.List(fields.Integer(required = True, 
+														default = "1,1,4,3,3,1,2,6,5,4,4,1",
+														description="Use this integer array of size 12 to test Evaluator model", 
+														help="Input Integer array cannot be blank or size less than 12."))})
+
+
+
+#model = {'input': fields.List(fields.int)}
+#resource_fields = {'name': fields.String, 'first_names': fields.List(fields.String)}
 
 #input:[1,1,4,3,3,1,2,6,5,4]
 #POST /simulator-model 
@@ -25,18 +44,22 @@ app = Api(app = flask_app,
 # response /simulator-model
 #{"category": 3}
 
-@app.route("/api/simulator-model")
+@name_space.route("/simulator-model")
 class SimulatorClass(Resource):
-	@app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' })
-	#@app.expect(model1)		
+	@app.doc(responses={ 200: 'OK', 400: 'Invalid Argument, Input needs to be Integer Array of Size 10', 500: 'Mapping Key Error' },
+		params={ 'input': 'Integer array of size 10 for simulator model' })
+	@app.expect(simulatorModel)
 	def post(self):
 		try:
-			simulatorClassifier = joblib.load("./models/rf_simulator.model")
+			print("Entry simulator classifier")
+			print("before joblib loading simulator classifier")
+			simulatorClassifier = joblib.load('./models/rf_simulator.model')
+			print("after joblib loading simulator classifier")
 			formData = request.get_json()
-			print(formData)
+			print("after reading input form data simulator classifier")
 			data = [val for val in formData.values()]
-			print(data)
 			prediction = simulatorClassifier.predict(np.array(data).reshape(1, -1))
+			print("after prediction simulator classifier")
 			print(prediction[0])
 			pred_str = prediction[0]
 			pred_int = int(pred_str)
@@ -44,37 +67,46 @@ class SimulatorClass(Resource):
 			return_value = {
 				'category' : pred_int
 			}
+			print("Exit simulator classifier")
 			return jsonify(return_value)
 		except KeyError as e:
+			print(e)
 			app.abort(500, e.__doc__, status = "Server Error", statusCode = "500")
 		except Exception as e:
 			print(e)
 			app.abort(400, e.__doc__, status = "Simulator Model Invalid Data", statusCode = "400")
 			
-			
-@app.route("/api/evaluator-model")
+@name_space.route("/evaluator-model")			
 class EvaluatorClass(Resource):
-	@app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' })
-	#@app.expect(model2)		
+	@app.doc(responses={ 200: 'OK', 400: 'Invalid Argument, Input needs to be Integer Array of Size 12', 500: 'Mapping Key Error' },
+		params={ 'input': 'Integer array of size 12 for Evaluator model' })
+	@app.expect(evaluatorModel)
 	def post(self):
 		try:
+			print("Entry evaluator classifier")
+			print("before joblib loading evaluator classifier")
 			evaluatorClassifier = joblib.load('./models/gb_lender.model')
+			print("after joblib loading evaluator classifier")
 			formData = request.get_json()
-			print(formData)
+			print("after reading input form data evaluator classifier")
 			data = [val for val in formData.values()]
-			print(data)
 			prediction = evaluatorClassifier.predict(np.array(data).reshape(1, -1))
+			print("after prediction evaluator classifier")
 			print(prediction[0])
 			pred_str = prediction[0]
+			#pred_str = "0"
 			pred_int = int(pred_str)
 			return_value = {}
 			return_value = {
 				'category' : pred_int
 			}
+			print("Exit evaluator classifier")
 			return jsonify(return_value)
 		except KeyError as e:
+			print(e)
 			app.abort(500, e.__doc__, status = "Could not save information", statusCode = "500")
 		except Exception as e:
+			print(e)
 			app.abort(400, e.__doc__, status = "Evaluate Model Invalid Data", statusCode = "400")
 
 if __name__=="__main__":
