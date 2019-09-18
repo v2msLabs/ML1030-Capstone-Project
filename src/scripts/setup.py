d4773bb3fa5b6075029d18696d6530edf0178bcb
Vadim Spirkov <vspirkov@starstechnologies.com>
2019-09-18 11:35:44 -0400

    fixed Python environment setup script

--- src/scripts/setup.py
+++ src/scripts/setup.py
@@ -1,9 +1,10 @@
 # this set-up file defines KASI Insight scripts to be installed as a module locally
 import setuptools
 
+
 setuptools.setup(
     name="kasi",
-    version="1.0.2",
+    version="1.0.4",
     packages=setuptools.find_packages(),
     description="This package comprises code to clean KASI Insight raw data and to tune and train the simulator and evaluator models",
     entry_points={
@@ -13,14 +14,11 @@ setuptools.setup(
                             ]
     },
     install_requires=[
-        'joblib>=0.13.0'
         'pandas>=0.24.1',
-        'numpy>=1.16.1',
+        'joblib>=0.13.0',
         'scipy>=1.2.0',
-        'scikit-learn>=0.20'
-        'imbalanced-learn>=0.5'
-    ],
-    extras_require={
-        'interactive': ['matplotlib>=3.0.0', 'jupyter', 'seaborn'],
-    }
+        'scikit-learn>=0.20',
+        'imbalanced-learn>=0.5',
+        'seaborn'
+    ]
 )
